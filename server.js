f4431a24d31bd4afee75a9223b1160e7ef5ed69a
Gautham Lal <gautham1995@gmail.com>
2018-12-31 02:05:49 +0530

    Can receive files of uploaded file

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ let port = process.env.PORT || 3000;
 
 const express = require('express');
 const cors = require('cors');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 let app = express();
 
@@ -11,8 +13,13 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-app.post('/api/fileanalyse', (req, res) => {
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  let file = req.file;
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  });
 });
 
 app.listen(port, () => {
